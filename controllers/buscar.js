fc4035f496d112540c5ad4b5ae074a1ccb917b69
fer <fer@fers-iMac.local>
2023-01-08 22:39:07 -0700

    koko

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -16,9 +16,7 @@ const buscarUsuarios = async( termino = '', res = response ) => {
 
     if ( esMongoID ) {
         const usuario = await Usuario.findById(termino);
-        return res.json({
-            results: ( usuario ) ? [ usuario ] : []
-        });
+        return res.json({ results: ( usuario ) ? [ usuario ] : [] });
     }
 
     const regex = new RegExp( termino, 'i' );
@@ -27,9 +25,7 @@ const buscarUsuarios = async( termino = '', res = response ) => {
         $and: [{ estado: true }]
     });
 
-    res.json({
-        results: usuarios
-    });
+    res.json({ results: usuarios });
 
 }
 
@@ -39,17 +35,13 @@ const buscarCategorias = async( termino = '', res = response ) => {
 
     if ( esMongoID ) {
         const categoria = await Categoria.findById(termino);
-        return res.json({
-            results: ( categoria ) ? [ categoria ] : []
-        });
+        return res.json({ results: ( categoria ) ? [ categoria ] : [] });
     }
 
     const regex = new RegExp( termino, 'i' );
     const categorias = await Categoria.find({ nombre: regex, estado: true });
 
-    res.json({
-        results: categorias
-    });
+    res.json({ results: categorias });
 
 }
 
@@ -58,55 +50,32 @@ const buscarProductos = async( termino = '', res = response ) => {
     const esMongoID = ObjectId.isValid( termino ); // TRUE 
 
     if ( esMongoID ) {
-        const producto = await Producto.findById(termino)
-                            .populate('categoria','nombre');
-        return res.json({
-            results: ( producto ) ? [ producto ] : []
-        });
+        const producto = await Producto.findById(termino).populate('categoria','nombre');
+        return res.json({ results: ( producto ) ? [ producto ] : []});
     }
 
     const regex = new RegExp( termino, 'i' );
-    const productos = await Producto.find({ nombre: regex, estado: true })
-                            .populate('categoria','nombre')
+    const productos = await Producto.find({ nombre: regex, estado: true }).populate('categoria','nombre')
 
-    res.json({
-        results: productos
-    });
+    res.json({ results: productos});
 
 }
-
 
 const buscar = ( req, res = response ) => {
     
     const { coleccion, termino  } = req.params;
 
     if ( !coleccionesPermitidas.includes( coleccion ) ) {
-        return res.status(400).json({
-            msg: `Las colecciones permitidas son: ${ coleccionesPermitidas }`
-        })
+        return res.status(400).json({ msg: `Las colecciones permitidas son: ${ coleccionesPermitidas }` })
     }
 
     switch (coleccion) {
-        case 'usuarios':
-            buscarUsuarios(termino, res);
-        break;
-        case 'categorias':
-            buscarCategorias(termino, res);
-        break;
-        case 'productos':
-            buscarProductos(termino, res);
-        break;
-
-        default:
-            res.status(500).json({
-                msg: 'Se le olvido hacer esta búsquda'
-            })
+        case 'usuarios': buscarUsuarios(termino, res); break;
+        case 'categorias': buscarCategorias(termino, res); break;
+        case 'productos': buscarProductos(termino, res); break;
+        default: res.status(500).json({ msg: 'Se le olvido hacer esta búsquda' })
     }
 
 }
 
-
-
-module.exports = {
-    buscar
-}
+module.exports = { buscar }
--- controllers/uploads.js
+++ controllers/uploads.js
@@ -6,114 +6,76 @@ cloudinary.config( process.env.CLOUDINARY_URL );
 
 const { response } = require('express');
 const { subirArchivo } = require('../helpers');
-
 const { Usuario, Producto } = require('../models');
 
-
 const cargarArchivo = async(req, res = response) => {
-
-
     try {
-        
-        // txt, md
-        // const nombre = await subirArchivo( req.files, ['txt','md'], 'textos' );
-        const nombre = await subirArchivo( req.files, undefined, 'imgs' );
-        res.json({ nombre });
-
+        const nombre = await subirArchivo( req.files, undefined, 'imgs' );  // txt, md
+        res.json({ nombre });  // const nombre = await subirArchivo( req.files, ['txt','md'], 'textos' );
     } catch (msg) {
         res.status(400).json({ msg });
     }
-
 }
-
 
 const actualizarImagen = async(req, res = response ) => {
 
     const { id, coleccion } = req.params;
-
     let modelo;
 
     switch ( coleccion ) {
-        case 'usuarios':
-            modelo = await Usuario.findById(id);
+        case 'usuarios': modelo = await Usuario.findById(id);
             if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
-                });
+                return res.status(400).json({msg: `No existe un usuario con el id ${ id }` });
             }
-        
         break;
 
-        case 'productos':
-            modelo = await Producto.findById(id);
+        case 'productos': modelo = await Producto.findById(id);
             if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un producto con el id ${ id }`
-                });
+                return res.status(400).json({ msg: `No existe un producto con el id ${ id }` });
             }
-        
         break;
     
-        default:
-            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+        default: return res.status(500).json({ msg: 'Se me olvidó validar esto'});
     }
 
 
-    // Limpiar imágenes previas
-    if ( modelo.img ) {
-        // Hay que borrar la imagen del servidor
+    
+    if ( modelo.img ) { // Limpiar imágenes previas
         const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
         if ( fs.existsSync( pathImagen ) ) {
-            fs.unlinkSync( pathImagen );
+            fs.unlinkSync( pathImagen ); // Hay que borrar la imagen del servidor
         }
     }
 
-
     const nombre = await subirArchivo( req.files, undefined, coleccion );
     modelo.img = nombre;
-
     await modelo.save();
-
-
     res.json( modelo );
-
 }
 
 
 const actualizarImagenCloudinary = async(req, res = response ) => {
 
     const { id, coleccion } = req.params;
-
     let modelo;
 
     switch ( coleccion ) {
-        case 'usuarios':
-            modelo = await Usuario.findById(id);
+        case 'usuarios': modelo = await Usuario.findById(id);
             if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
-                });
+                return res.status(400).json({ msg: `No existe un usuario con el id ${ id }` });
             }
-        
         break;
 
-        case 'productos':
-            modelo = await Producto.findById(id);
+        case 'productos': modelo = await Producto.findById(id);
             if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un producto con el id ${ id }`
-                });
+                return res.status(400).json({ msg: `No existe un producto con el id ${ id }` });
             }
-        
         break;
     
-        default:
-            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+        default: return res.status(500).json({ msg: 'Se me olvidó validar esto'});
     }
 
-
-    // Limpiar imágenes previas
-    if ( modelo.img ) {
+    if ( modelo.img ) { // Limpiar imágenes previas
         const nombreArr = modelo.img.split('/');
         const nombre    = nombreArr[ nombreArr.length - 1 ];
         const [ public_id ] = nombre.split('.');
@@ -123,52 +85,36 @@ const actualizarImagenCloudinary = async(req, res = response ) => {
 
     const { tempFilePath } = req.files.archivo
     const { secure_url } = await cloudinary.uploader.upload( tempFilePath );
+    
     modelo.img = secure_url;
-
     await modelo.save();
-
-
     res.json( modelo );
-
 }
 
 const mostrarImagen = async(req, res = response ) => {
 
     const { id, coleccion } = req.params;
-
     let modelo;
 
     switch ( coleccion ) {
-        case 'usuarios':
-            modelo = await Usuario.findById(id);
+        case 'usuarios': modelo = await Usuario.findById(id);
             if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
-                });
+                return res.status(400).json({ msg: `No existe un usuario con el id ${ id }` });
             }
-        
         break;
 
-        case 'productos':
-            modelo = await Producto.findById(id);
+        case 'productos': modelo = await Producto.findById(id);
             if ( !modelo ) {
-                return res.status(400).json({
-                    msg: `No existe un producto con el id ${ id }`
-                });
+                return res.status(400).json({ msg: `No existe un producto con el id ${ id }` });
             }
-        
         break;
     
-        default:
-            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+        default: return res.status(500).json({ msg: 'Se me olvidó validar esto'});
     }
 
-
-    // Limpiar imágenes previas
-    if ( modelo.img ) {
-        // Hay que borrar la imagen del servidor
+    if ( modelo.img ) {    // Limpiar imágenes previas
         const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
-        if ( fs.existsSync( pathImagen ) ) {
+        if ( fs.existsSync( pathImagen ) ) {         // Hay que borrar la imagen del servidor
             return res.sendFile( pathImagen )
         }
     }
@@ -177,12 +123,4 @@ const mostrarImagen = async(req, res = response ) => {
     res.sendFile( pathImagen );
 }
 
-
-
-
-module.exports = {
-    cargarArchivo,
-    actualizarImagen,
-    mostrarImagen,
-    actualizarImagenCloudinary
-}
+module.exports = { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary }
